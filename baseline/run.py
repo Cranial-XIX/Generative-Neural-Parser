2de8447905020f2b7212f0d8bf942986331ecdf5
VermicelliInCasserole <bliu24@jhu.edu>
2017-05-10 00:59:16 -0400

    finally got everythin in cython and the parsing speed is 1.6 seconds for 10 sentences, including inside-outside, pruning and parsing together

--- baseline/run.py
+++ baseline/run.py
@@ -9,44 +9,38 @@ def run(GrammarObject):
     t1 = time.time()
     #print "Reading grammar takes %.4f secs" % round(t1 - t0, 5)
 
-    grammer_obj.compute_sum_and_max_of_unary_combos()
-    t2 = time.time()
-    #print "Unary combos take %.4f secs" % round(t2 - t1, 5)
-    threshold = 1e-7
-
-    grammer_obj.prune_unlikely_rules_and_lexicon(threshold)
-    t3 = time.time()
-    #print "Pruning rules and lexicon takes %.4f secs" % round(t3 - t2, 5)
+    print t1 - t0
 
     begin = time.time()
     test = "../data/corpus/test.sen"
     with open(test, 'r') as file:
         i = 0
         for sentence in file:
-            if i == 5:
+            if i == 10:
                 break
             i += 1
-            log_prob_sentence = grammer_obj.do_inside_outside(sentence)
-            #t4 = time.time()
+            prob_sen = grammer_obj.do_inside_outside(sentence)
+
             #print "Inside-outside takes %.4f secs" % round(t4 - t3, 5)
 
-            posterior_threshold = 0
-            grammer_obj.prune_the_chart(sentence, log_prob_sentence, posterior_threshold)
+            posterior_threshold = 1e-12
+            grammer_obj.prune_the_chart(prob_sen, posterior_threshold)
             #t5 = time.time()
             #print "Pruning takes %.4f secs" % round(t5 - t4, 5)
 
-            str = grammer_obj.parse(sentence)
+            grammer_obj.parse(sentence)
             #t6 = time.time()
             #print "Parsing takes %.4f secs\n" % round(t6 - t5, 5)
 
-            print "log of Pr( ", "sentence", ") = ", log_prob_sentence
-            print grammer_obj.debinarize(str)
+            print "log of Pr( ", sentence.strip(), ") = ", prob_sen, "\n\n"
             
             # Debug
             # grammer_obj.validate_read_grammar()
 
+
     end = time.time()
     print "Parsing takes %.4f secs\n" % round(end - begin, 5)
+
 if __name__=="__main__":
     run(gr.GrammarObject)
     
